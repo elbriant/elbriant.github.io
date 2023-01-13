a8e4d3bf7e175f9fc464e4caf4e0177ff214423c
elbriant <64721042+elbriant@users.noreply.github.com>
2023-01-13 01:20:48 +0000

    version final de contactme.html, y casi version final de index

--- contactme.js
+++ contactme.js
@@ -1,9 +1,22 @@
 function sendEmail(){
+  var date = new Date();
+	var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
+	var current_time = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
+	var date_time = current_date+" "+current_time;	
+
+  var numberPhone = document.getElementById("number").value || "not set";
+
   var params = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
+    number: numberPhone,
     subject: document.getElementById("subject").value,
     bodytext: document.getElementById("bodytext").value,
+    time: date_time,
+  };
+
+  if (params[number] === undefined ) {
+    params[number] == "Not set";
   };
 
   const serviceID = "service_ghcm1bk";
